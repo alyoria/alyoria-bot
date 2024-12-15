7f1cc0de2e38dbeb0edef4c6f06ad421c1e2fa54
Plhume <72684309+Plhume@users.noreply.github.com>
2024-12-15 15:21:23 +0100

    Create ecosystem.config.js

---
+++ ecosystem.config.js
@@ -0,0 +1,18 @@
+{
+    "apps": [
+      {
+        "name": "alyoria-bot",
+        "script": "npm",
+        "args": "run dev",
+        "interpreter": "none",
+        "watch": false,
+        "env": {
+          "NODE_ENV": "development"
+        },
+        "env_production": {
+          "NODE_ENV": "production"
+        }
+      }
+    ]
+  }
+  
