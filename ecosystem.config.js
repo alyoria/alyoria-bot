3f1b329f11953597d1705c69c08010fb39448efa
Plhume <72684309+Plhume@users.noreply.github.com>
2024-12-15 18:21:40 +0100

    Update ecosystem.config.js

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,18 +1,17 @@
-{
-    "apps" [
-      {
-        "name": "alyoria-bot",
-        "script": "npm",
-        "args": "run dev",
-        "interpreter": "none",
-        "watch": false,
-        "env": {
-          "NODE_ENV": "development"
-        },
-        "env_production": {
-          "NODE_ENV": "production"
-        }
-      }
-    ]
-  }
-  
+module.exports = {
+  apps: [
+    {
+      name: "alyoria-bot",
+      script: "npm",
+      args: "run dev",
+      interpreter: "none",
+      watch: false,
+      env: {
+        NODE_ENV: "development",
+      },
+      env_production: {
+        NODE_ENV: "production",
+      },
+    },
+  ],
+};
